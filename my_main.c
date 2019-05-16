f0320615df03960ee354ab85c6e69a71fd02f930
camillac <ccheng6@stuy.edu>
2019-05-16 00:03:18 -0400

    test

--- my_main.c
+++ my_main.c
@@ -1,36 +1,26 @@
 /*========== my_main.c ==========
-
   This is the only file you need to modify in order
   to get a working mdl project (for now).
-
   my_main.c will serve as the interpreter for mdl.
   When an mdl script goes through a lexer and parser,
   the resulting operations will be in the array op[].
-
   Your job is to go through each entry in op and perform
   the required action from the list below:
-
   push: push a new origin matrix onto the origin stack
-
   pop: remove the top matrix on the origin stack
-
   move/scale/rotate: create a transformation matrix
                      based on the provided values, then
                      multiply the current top of the
                      origins stack by it.
-
   box/sphere/torus: create a solid object based on the
                     provided values. Store that in a
                     temporary matrix, multiply it by the
                     current top of the origins stack, then
                     call draw_polygons.
-
   line: create a line based on the provided values. Store
         that in a temporary matrix, multiply it by the
         current top of the origins stack, then call draw_lines.
-
   save: call save_extension with the provided filename
-
   display: view the screen
   =========================*/
 
@@ -53,11 +43,13 @@ void my_main() {
 
   int i;
   struct matrix *tmp;
+  struct matrix *edges;
+  struct matrix *polygons;
   struct stack *systems;
   screen t;
   zbuffer zb;
   color g;
-  double step_3d = 20;
+  double step_3d = 200;
   double theta;
 
   //Lighting values here for easy access
@@ -71,7 +63,7 @@ void my_main() {
   light[LOCATION][1] = 0.75;
   light[LOCATION][2] = 1;
 
-  light[COLOR][RED] = 0;
+  light[COLOR][RED] = 255;
   light[COLOR][GREEN] = 255;
   light[COLOR][BLUE] = 255;
 
@@ -100,6 +92,8 @@ void my_main() {
 
   systems = new_stack();
   tmp = new_matrix(4, 1000);
+  polygons = new_matrix(4,4);
+  edges = new_matrix(4,4);
   clear_screen( t );
   clear_zbuffer(zb);
   g.red = 0;
@@ -110,7 +104,133 @@ void my_main() {
   for (i=0;i<lastop;i++) {
 
     printf("%d: ",i);
-
+    switch(op[i].opcode){
+      //PUSH
+    case PUSH:
+      printf("PUSH");
+      push(systems);
+      break;
+      //POP
+    case POP:
+      printf("POP");
+      pop(systems);
+      break;
+      //MOVE
+    case MOVE:
+      printf("MOVE");
+      tmp = make_translate(op[i].op.move.d[0],
+			   op[i].op.move.d[1],
+			   op[i].op.move.d[2]);
+      matrix_mult(peek(systems),tmp);
+      copy_matrix(tmp, peek(systems));
+      break;
+      //ROTATE
+    case ROTATE:
+      printf("ROTATE");
+      theta = op[i].op.rotate.degrees * (M_PI / 180);
+      if(op[i].op.rotate.axis == 0.0){
+	tmp = make_rotX(theta);
+      }
+      else if(op[i].op.rotate.axis == 1.0){
+	tmp = make_rotY(theta);
+      }
+      else{
+	tmp = make_rotZ(theta);
+      }
+      matrix_mult(peek(systems), tmp);
+      copy_matrix(tmp, peek(systems));
+      break;
+      //SCALE
+    case SCALE:
+      printf("SCALE");
+      tmp = make_scale(op[i].op.scale.d[0],
+		       op[i].op.scale.d[1],
+		       op[i].op.scale.d[2]);
+      matrix_mult(peek(systems),tmp);
+      copy_matrix(tmp, peek(systems));
+      break;
+      //BOX
+    case BOX:
+      printf("BOX");
+      add_box(polygons,
+	      op[i].op.box.d0[0],
+	      op[i].op.box.d0[1],
+	      op[i].op.box.d0[2],
+	      op[i].op.box.d1[0],
+	      op[i].op.box.d1[1],
+	      op[i].op.box.d1[2]);
+      matrix_mult(peek(systems), polygons);
+      reflect = &white;
+      if(op[i].op.box.constants != NULL){
+	reflect = op[i].op.box.constants->s.c;
+      }
+      draw_polygons(polygons, t, zb, view, light, ambient, reflect);
+      polygons->lastcol = 0;
+      break;
+      //SPHERE
+    case SPHERE:
+      printf("SPHERE");
+      add_sphere(polygons,
+		 op[i].op.sphere.d[0],
+		 op[i].op.sphere.d[1],
+		 op[i].op.sphere.d[2],
+		 op[i].op.sphere.r,
+		 step_3d);
+      matrix_mult(peek(systems), polygons);
+      reflect = &white;
+      if(op[i].op.sphere.constants != NULL){
+	reflect = op[i].op.sphere.constants->s.c;
+      }
+      draw_polygons(polygons, t, zb, view, light, ambient, reflect);
+      polygons->lastcol = 0;
+      break;
+      //TORUS
+    case TORUS:
+      printf("TORUS");
+      add_torus(polygons,
+		op[i].op.torus.d[0],
+		op[i].op.torus.d[1],
+		op[i].op.torus.d[2],
+		op[i].op.torus.r0,
+		op[i].op.torus.r1,
+		step_3d);
+      matrix_mult(peek(systems), polygons);
+      reflect = &white;
+      if (op[i].op.torus.constants != NULL){
+	reflect = op[i].op.torus.constants->s.c;
+      }
+      draw_polygons(polygons, t, zb, view, light, ambient, reflect);
+      polygons->lastcol = 0;
+      break;
+      //CONSTANTS
+    case CONSTANTS:
+      printf("CONSTANTS");
+      break;
+      //LINE
+    case LINE:
+      printf("LINE");
+      add_edge(edges,
+	       op[i].op.line.p0[0],
+	       op[i].op.line.p0[1],
+	       op[i].op.line.p0[2],
+	       op[i].op.line.p1[0],
+	       op[i].op.line.p1[1],
+	       op[i].op.line.p1[2]);
+      matrix_mult(peek(systems),edges);
+      draw_lines(edges, t, zb, g);
+      edges->lastcol = 0;
+      break;
+      //SAVE
+    case SAVE:
+      printf("SAVE");
+      save_extension(t, op[i].op.save.p->name);
+      break;
+      //DISPLAY
+    case DISPLAY:
+      printf("DISPLAY");
+      display(t);
+      break;
+    }
     printf("\n");
   }
 }
